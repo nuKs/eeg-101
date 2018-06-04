31d6fb7e067dfe98da37a5c4dcb064f988a4f898
Thibault Piront nuKs <gse.nuks@gmail.com>
2018-06-04 18:04:06 +0200

    experimental vertical scrolllist (snap not working)

--- EEG101/src/bimslab/experiments/ExperimentQAScene.js
+++ EEG101/src/bimslab/experiments/ExperimentQAScene.js
@@ -4,7 +4,7 @@
 
 // React-native
 import React, { Component } from 'react';
-import { View, Slider, FlatList } from "react-native";
+import { View, Slider, FlatList, TouchableHighlight, Dimensions,  } from "react-native";
 import styled from "styled-components";
 import { Text, Button, Icon } from 'native-base';
 
@@ -37,6 +37,32 @@ const HeaderText_ = styled(Text)
     padding-right: 10;
   `;
 
+let _data = [
+  {
+    q: 'Ma question #1'
+  },
+  {
+    q: 'Ma question #2'
+  },
+  {
+    q: 'Ma question #3'
+  },
+  {
+    q: 'Ma question #4'
+  },
+  {
+    q: 'Ma question #5'
+  },
+  {
+    q: 'Ma question #6'
+  },
+  {
+    q: 'Ma question #7'
+  },
+];
+
+const {WIN_HEIGHT, WIN_WIDTH} = Dimensions.get('window');
+
 class ExperimentQAScene extends Component {
   constructor(props) {
     super(props)
@@ -49,6 +75,7 @@ class ExperimentQAScene extends Component {
       answer5: 0.5,
       answer6: 0.5,
       answer7: 0.5,
+      dimensions: {}
     }
 
     this.submit = this.submit.bind(this);
@@ -62,71 +89,48 @@ class ExperimentQAScene extends Component {
     this.props.history.push('/experiment/connector/1');
   }
 
+  onLayout = event => {
+    // if (this.state.dimensions) return // layout was already called
+    let {width, height} = event.nativeEvent.layout;
+    this.setState({dimensions: {width, height}})
+  }
+
   // @todo set state change
   render() {
     // @todo move flatlist out of render fn
     return (
       <Wrapper_>
-        <View style={{flex: 1, justifyContent: 'center'}}>
+        {/*flex: 1, */}
+        <View style={{flex: 0, justifyContent: 'center'}}>
           <HeaderText_>Indiquez la sévérité de vos symptomes.</HeaderText_>
         </View>
-        <View style={{flex: 1, justifyContent: 'center'}}>
-          <HeaderText_>Ma question #1</HeaderText_>
-          <View style={{flex: 1, flexDirection: 'row', justifyContent: 'center'}}>
-            <Text style={{padding: 20, justifyContent: 'center'}}>-</Text>
-            <Slider value={this.state.answer1} style={{flex: 1}} onChange={evt => this.setState({answer1: evt.target.value})} />
-            <Text style={{padding: 20, justifyContent: 'center'}}>+</Text>
-          </View>
+        <View
+          style={{flex: 1}}
+          onLayout={this.onLayout}
+        >
+          {this.state.dimensions.height && 
+          <FlatList
+            style={{height: this.state.dimensions.height}}
+            data={_data}
+           bounces={false}
+           snapToInterval={Math.round(this.state.dimensions.height/3)}
+            snapToAlignment={'start'}
+            decelerationRate={'fast'}
+            pagingEnabled={false}
+            keyExtractor={(item, idx) => `${idx}`}
+            renderItem={({item}) => 
+              <TouchableHighlight>
+                <View style={{minHeight: this.state.dimensions.height/3, backgroundColor: 'red'}}>
+                  <Text>{item.q}
+                  </Text>
+                </View>
+              </TouchableHighlight>
+            }
+          />}
         </View>
-        <View style={{flex: 1, justifyContent: 'center'}}>
-          <HeaderText_>Ma question #2</HeaderText_>
-          <View style={{flex: 1, flexDirection: 'row', justifyContent: 'center'}}>
-            <Text style={{padding: 20, justifyContent: 'center'}}>-</Text>
-            <Slider value={this.state.answer2} style={{flex: 1}} onChange={evt => this.setState({answer2: evt.target.value})} />
-            <Text style={{padding: 20, justifyContent: 'center'}}>+</Text>
-          </View>
-        </View>
-        <View style={{flex: 1, justifyContent: 'center'}}>
-          <HeaderText_>Ma question #3</HeaderText_>
-          <View style={{flex: 1, flexDirection: 'row', justifyContent: 'center'}}>
-            <Text style={{padding: 20, justifyContent: 'center'}}>-</Text>
-            <Slider value={this.state.answer3} style={{flex: 1}} onChange={evt => this.setState({answer3: evt.target.value})} />
-            <Text style={{padding: 20, justifyContent: 'center'}}>+</Text>
-          </View>
-        </View>
-        <View style={{flex: 1, justifyContent: 'center'}}>
-          <HeaderText_>Ma question #4</HeaderText_>
-          <View style={{flex: 1, flexDirection: 'row', justifyContent: 'center'}}>
-            <Text style={{padding: 20, justifyContent: 'center'}}>-</Text>
-            <Slider value={this.state.answer4} style={{flex: 1}} onChange={evt => this.setState({answer4: evt.target.value})} />
-            <Text style={{padding: 20, justifyContent: 'center'}}>+</Text>
-          </View>
-        </View>
-        <View style={{flex: 1, justifyContent: 'center'}}>
-          <HeaderText_>Ma question #5</HeaderText_>
-          <View style={{flex: 1, flexDirection: 'row', justifyContent: 'center'}}>
-            <Text style={{padding: 20, justifyContent: 'center'}}>-</Text>
-            <Slider value={this.state.answer5} style={{flex: 1}} onChange={evt => this.setState({answer5: evt.target.value})} />
-            <Text style={{padding: 20, justifyContent: 'center'}}>+</Text>
-          </View>
-        </View>
-        <View style={{flex: 1, justifyContent: 'center'}}>
-          <HeaderText_>Ma question #6</HeaderText_>
-          <View style={{flex: 1, flexDirection: 'row', justifyContent: 'center'}}>
-            <Text style={{padding: 20, justifyContent: 'center'}}>-</Text>
-            <Slider value={this.state.answer6} style={{flex: 1}} onChange={evt => this.setState({answer6: evt.target.value})} />
-            <Text style={{padding: 20, justifyContent: 'center'}}>+</Text>
-          </View>
-        </View>
-        <View style={{flex: 1, justifyContent: 'center'}}>
-          <HeaderText_>Ma question #7</HeaderText_>
-          <View style={{flex: 1, flexDirection: 'row', justifyContent: 'center'}}>
-            <Text style={{padding: 20, justifyContent: 'center'}}>-</Text>
-            <Slider value={this.state.answer7} style={{flex: 1}} onChange={evt => this.setState({answer7: evt.target.value})} />
-            <Text style={{padding: 20, justifyContent: 'center'}}>+</Text>
-          </View>
-        </View>
-        <View style={{flex: 1, justifyContent: 'center'}}>
+
+        {/*flex: 1, */}
+        <View style={{flex: 0, justifyContent: 'center'}}>
           <Button full success onPress={this.submit}>
             <Icon name="checkmark-circle" />
             <Text>Valider</Text>
@@ -136,6 +140,64 @@ class ExperimentQAScene extends Component {
     );
   }
 }
+
+
+  // <View style={{flex: 1, justifyContent: 'center'}}>
+  //   <HeaderText_>Ma question #1</HeaderText_>
+  //   <View style={{flex: 1, flexDirection: 'row', justifyContent: 'center'}}>
+  //     <Text style={{padding: 20, justifyContent: 'center'}}>-</Text>
+  //     <Slider value={this.state.answer1} style={{flex: 1}} onChange={evt => this.setState({answer1: evt.target.value})} />
+  //     <Text style={{padding: 20, justifyContent: 'center'}}>+</Text>
+  //   </View>
+  // </View>
+  // <View style={{flex: 1, justifyContent: 'center'}}>
+  //   <HeaderText_>Ma question #2</HeaderText_>
+  //   <View style={{flex: 1, flexDirection: 'row', justifyContent: 'center'}}>
+  //     <Text style={{padding: 20, justifyContent: 'center'}}>-</Text>
+  //     <Slider value={this.state.answer2} style={{flex: 1}} onChange={evt => this.setState({answer2: evt.target.value})} />
+  //     <Text style={{padding: 20, justifyContent: 'center'}}>+</Text>
+  //   </View>
+  // </View>
+  // <View style={{flex: 1, justifyContent: 'center'}}>
+  //   <HeaderText_>Ma question #3</HeaderText_>
+  //   <View style={{flex: 1, flexDirection: 'row', justifyContent: 'center'}}>
+  //     <Text style={{padding: 20, justifyContent: 'center'}}>-</Text>
+  //     <Slider value={this.state.answer3} style={{flex: 1}} onChange={evt => this.setState({answer3: evt.target.value})} />
+  //     <Text style={{padding: 20, justifyContent: 'center'}}>+</Text>
+  //   </View>
+  // </View>
+  // <View style={{flex: 1, justifyContent: 'center'}}>
+  //   <HeaderText_>Ma question #4</HeaderText_>
+  //   <View style={{flex: 1, flexDirection: 'row', justifyContent: 'center'}}>
+  //     <Text style={{padding: 20, justifyContent: 'center'}}>-</Text>
+  //     <Slider value={this.state.answer4} style={{flex: 1}} onChange={evt => this.setState({answer4: evt.target.value})} />
+  //     <Text style={{padding: 20, justifyContent: 'center'}}>+</Text>
+  //   </View>
+  // </View>
+  // <View style={{flex: 1, justifyContent: 'center'}}>
+  //   <HeaderText_>Ma question #5</HeaderText_>
+  //   <View style={{flex: 1, flexDirection: 'row', justifyContent: 'center'}}>
+  //     <Text style={{padding: 20, justifyContent: 'center'}}>-</Text>
+  //     <Slider value={this.state.answer5} style={{flex: 1}} onChange={evt => this.setState({answer5: evt.target.value})} />
+  //     <Text style={{padding: 20, justifyContent: 'center'}}>+</Text>
+  //   </View>
+  // </View>
+  // <View style={{flex: 1, justifyContent: 'center'}}>
+  //   <HeaderText_>Ma question #6</HeaderText_>
+  //   <View style={{flex: 1, flexDirection: 'row', justifyContent: 'center'}}>
+  //     <Text style={{padding: 20, justifyContent: 'center'}}>-</Text>
+  //     <Slider value={this.state.answer6} style={{flex: 1}} onChange={evt => this.setState({answer6: evt.target.value})} />
+  //     <Text style={{padding: 20, justifyContent: 'center'}}>+</Text>
+  //   </View>
+  // </View>
+  // <View style={{flex: 1, justifyContent: 'center'}}>
+  //   <HeaderText_>Ma question #7</HeaderText_>
+  //   <View style={{flex: 1, flexDirection: 'row', justifyContent: 'center'}}>
+  //     <Text style={{padding: 20, justifyContent: 'center'}}>-</Text>
+  //     <Slider value={this.state.answer7} style={{flex: 1}} onChange={evt => this.setState({answer7: evt.target.value})} />
+  //     <Text style={{padding: 20, justifyContent: 'center'}}>+</Text>
+  //   </View>
+  // </View>
 
 // Bind redux store to react component
 function mapStateToProps(state) {
