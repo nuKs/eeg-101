658bb60c2dc815528aeea85889be042e961d3f0e
Thibault Piront nuKs <gse.nuks@gmail.com>
2018-07-04 12:54:39 +0200

    sync?

--- EEG101/android/app/src/main/java/com/eeg_project/AwareModule.java
+++ EEG101/android/app/src/main/java/com/eeg_project/AwareModule.java
@@ -1,9 +1,15 @@
 package com.eeg_project;
 
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.IBinder;
 import android.util.Log;
 
 import com.aware.Aware;
 import com.aware.Aware_Preferences;
+import com.aware.plugin.eegmuse.Plugin;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
@@ -13,6 +19,7 @@ import java.util.Map;
 
 public class AwareModule extends ReactContextBaseJavaModule {
     private ReactApplicationContext _reactContext;
+    private Plugin _plugin;
 
     public AwareModule(ReactApplicationContext reactContext) {
         super(reactContext);
@@ -25,7 +32,7 @@ public class AwareModule extends ReactContextBaseJavaModule {
         Aware.setSetting(reactContext, Aware_Preferences.DEBUG_FLAG, "true");
         Aware.setSetting(reactContext, Aware_Preferences.DEBUG_TAG, "AWARE");
         Aware.joinStudy(reactContext, "https://api.awareframework.com/index.php/webservice/index/1801/C7DzyHpq2phjd"); //TODO: UPDATE to UCLA server dashboard study endpoint
-
+        
         // @todo fix db location @warning breaks launch time // may have been done
         /*
         Log.i("AwareModule", "start com.aware.plugin.eegmuse");
@@ -51,50 +58,60 @@ public class AwareModule extends ReactContextBaseJavaModule {
     protected ServiceConnection mServerConn = new ServiceConnection() {
         @Override
         public void onServiceConnected(ComponentName name, IBinder binder) {
-            Log.d(LOG_TAG, "onServiceConnected");
+            Log.d("AwareModule", "onServiceConnected");
+            /*
+            Plugin.LocalBinder binder2 = binder;
+            _plugin = binder.getService();
+            */
         }
 
         @Override
         public void onServiceDisconnected(ComponentName name) {
-            Log.d(LOG_TAG, "onServiceDisconnected");
+            Log.d("AwareModule", "onServiceDisconnected");
         }
-    }
+    };
 
     @ReactMethod
-    public void startRecord() {
+    public void startPlugin() {
         Log.i("AwareModule", "bridge/java: startRecord");
         Aware.startPlugin(this._reactContext, "com.aware.plugin.eegmuse");
 
-        // mContext is defined upper in code, I think it is not necessary to explain
-        // what is it
         ComponentName componentName;
+        componentName = new ComponentName(_reactContext.getPackageName(), "com.aware.plugin.eegmuse.Plugin");
+        if (Aware.DEBUG)
+            Log.d(Aware.TAG, "Initializing bundled: " + componentName.toString());
+
+        /*
         if (packageInfo.versionName.equals("bundled")) {
-            componentName = new ComponentName(context.getPackageName(), "com.aware.plugin.eegmuse.Plugin");
-            if (Aware.DEBUG)
-                Log.d(Aware.TAG, "Initializing bundled: " + componentName.toString());
         } else {
             componentName = new ComponentName("com.aware.plugin.eegmuse", "com.aware.plugin.eegmuse.Plugin");
             if (Aware.DEBUG)
                 Log.d(Aware.TAG, "Initializing external: " + componentName.toString());
         }
+        */
 
         Intent pluginIntent = new Intent();
         pluginIntent.setComponent(componentName);
 
-        mContext.bindService(intent, mServerConn, Context.BIND_AUTO_CREATE);
+        _reactContext.bindService(pluginIntent, mServerConn, Context.BIND_AUTO_CREATE);
+    }
+
+    @ReactMethod
+    public void stopPlugin() {
+        // @pre-cond stopRecord()
+        Log.i("AwareModule", "bridge/java: stopRecord");
+        Aware.stopPlugin(this._reactContext, "com.aware.plugin.eegmuse");
+        _reactContext.unbindService(mServerConn);
+    }
+
+    @ReactMethod
+    public void startRecord() {
+        // @pre-cond startPlugin()
+        _plugin.startRecording();
     }
 
     @ReactMethod
     public void stopRecord() {
-        Log.i("AwareModule", "bridge/java: stopRecord");
-        Aware.stopPlugin(this._reactContext, "com.aware.plugin.eegmuse");
-        mContext.unbindService(mServerConn);
+        _plugin.stopRecording();
     }
-
-    /*
-    @ReactMethod
-    public void show(String message, int duration) {
-        Toast.makeText(getReactApplicationContext(), message, duration).show();
-    }
-    */
 }
